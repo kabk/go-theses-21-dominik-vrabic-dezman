1ecaf4eb66800d68423eca376cdc2499bc083000
dmnkvd <dominikvr@gmail.com>
2021-01-24 09:05:45 +0100

    fixed JS

--- js/dvd.js
+++ js/dvd.js
@@ -24,6 +24,7 @@ window.addEventListener("DOMContentLoaded", (event) => {
   });
 
   const image = document.querySelectorAll("figure img");
+  console.log(image);
 
   const darkSelector = document.querySelectorAll(".mode--dark");
 
